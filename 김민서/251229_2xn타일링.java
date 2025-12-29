9c9b4770140baec0efdc27e038a570a6a1cf1385
minseokim0113 <minseokim0113@gmail.com>
2025-12-29 17:24:33 +0900

    feat: [김민서][251229] 2xn 타일링 제출

---
+++ 김민서/251229_2xn타일링.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+            if(i <= 2) {
+                dp[i] = i;
+            } else {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+            }
+        }
+        return dp[n];
+    }
+}
