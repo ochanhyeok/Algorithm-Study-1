fe83edd204076ccc403f807181f5d55f750e9b18
gaeunnlee <gaeunnnleee@gmail.com>
2026-02-12 23:37:57 +0900

    fix: [이가은][260212] 신입사원 교육 수정

--- 이가은/260212_신입사원 교육.java
+++ 이가은/260212_신입사원 교육.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] ability, int number) {
-        for (int i = 0; i < number; i++) {
-            Arrays.sort(ability);
-            
-            int sum = ability[0] + ability[1];
-            ability[0] = sum;
-            ability[1] = sum;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n: ability) {
+            pq.add(n);
         }
         
-        return Arrays.stream(ability).sum();
+        for (int i = 0; i < number; i++) {
+            int sum = pq.poll() + pq.poll();
+            pq.add(sum);
+            pq.add(sum);
+        }
+        
+        int sum = 0;
+        while(!pq.isEmpty()) sum += pq.poll();
+        return sum;
     }
 }
