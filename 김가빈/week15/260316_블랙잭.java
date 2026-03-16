df7cb4572b976c74d9cd8d7060335ce48e637688
Gabee-ni <been.dev,kim@gmail.com>
2026-03-16 18:06:19 +0900

    fix: [김가빈][260316] 블랙잭 수정

--- 김가빈/week15/260316_블랙잭.java
+++ 김가빈/week15/260316_블랙잭.java
@@ -1,52 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int N;
-    static double[] percent = new double[4]; // 동, 서, 남, 북
-    static boolean[][] visited = new boolean[30][30];
-    static double answer = 0.0;
+class Main {
+    static int N, M;
+    static int[] arr;
+    static int answer = 0;
 
-    // 동, 서, 남, 북
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
 
-        for (int i = 0; i < 4; i++) {
-            percent[i] = Integer.parseInt(st.nextToken()) / 100.0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int startX = 15;
-        int startY = 15;
-        visited[startX][startY] = true;
-
-        dfs(startX, startY, 0, 1.0);
-
+        dfs(0, 0, 0);
         System.out.println(answer);
     }
-
-    static void dfs(int x, int y, int depth, double prob) {
-        if (depth == N) {
-            answer += prob;
+    
+    static void dfs(int idx, int count, int sum) {
+        if(count == 3 ){
+            if(sum <= M) {
+                //최대한 M에 가깝게
+                answer = Math.max(answer, sum);    
+            }
             return;
-        }
-
-        for (int dir = 0; dir < 4; dir++) {
-            if (percent[dir] == 0) continue;
-
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if (visited[nx][ny]) continue;
-
-            visited[nx][ny] = true;
-            dfs(nx, ny, depth + 1, prob * percent[dir]);
-            visited[nx][ny] = false;
+        } 
+        
+        //고른 카드에서부터 뽑기 
+        for(int i=idx; i<N; i++){
+            dfs(i+1, count+1, sum + arr[i]);
         }
     }
 }
